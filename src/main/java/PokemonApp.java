1f70dae78ba817db75c9a64df56ee2a71811704b
sbranns <sambranns@gmail.com>
2024-07-22 16:43:55 -0400

    add view

--- src/main/java/PokemonApp.java
+++ src/main/java/PokemonApp.java
@@ -6,21 +6,23 @@ import java.io.IOException; // Add this import statement
 import org.json.simple.parser.ParseException; // Add this import statement
 
 import student.model.NetUtils;
+import student.view.PokedexView;
 
 public class PokemonApp {
     public static void main (String[] args) {
-        InputStream inputStream = NetUtils.getIpDetails("pikachu");
-        JSONParser jsonParser = new JSONParser();
-        try {
-            try {
-                JSONObject jsonObject = (JSONObject)jsonParser.parse(
-                    new InputStreamReader(inputStream, "UTF-8"));
-                    System.out.println(jsonObject.get("name"));
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-        } catch (IOException e ) {
-            e.printStackTrace();
-        }
+        PokedexView view = new PokedexView();
+        // InputStream inputStream = NetUtils.getIpDetails("pikachu");
+        // JSONParser jsonParser = new JSONParser();
+        // try {
+        //     try {
+        //         JSONObject jsonObject = (JSONObject)jsonParser.parse(
+        //             new InputStreamReader(inputStream, "UTF-8"));
+        //             System.out.println(jsonObject.get("name"));
+        //     } catch (ParseException e) {
+        //         e.printStackTrace();
+        //     }
+        // } catch (IOException e ) {
+        //     e.printStackTrace();
+        // }
     }
 }
