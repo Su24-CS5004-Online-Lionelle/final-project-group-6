43a40718e748302d4cab025c9618e275cfcaefc9
mzecic <matejzecic@gmail.com>
2024-07-27 07:10:43 -0400

    Added individual item for each pokemon, enabled highlighting

--- src/main/java/student/view/PokemonListPanel.java
+++ src/main/java/student/view/PokemonListPanel.java
@@ -1,8 +1,43 @@
 package student.view;
 import javax.swing.*;
+
+import org.checkerframework.checker.units.qual.s;
+
+import java.util.ArrayList;
+import java.util.List; // Add this import statement
+
+import student.controller.PokedexController;
+import student.model.PokeRecord;
+import student.view.Components.CustomPanel;
+
 import java.awt.*;
 
 public class PokemonListPanel extends JPanel {
+        PokedexController controller = new PokedexController();
+        List<PokeRecord> pokemonList;
+        List<CustomPanel> customRectList = new ArrayList<>();
+        JPanel listPanel = new JPanel();
+
+        public PokemonListPanel() {
+            setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+            pokemonList = controller.getAllPokemon();
+            for (PokeRecord pokemon : pokemonList) {
+                customRectList.add(new CustomPanel(pokemon.name()));
+            }
+
+            for (CustomPanel item : customRectList) {
+                listPanel.add(item);
+            }
+
+            listPanel.setLayout(new BoxLayout(listPanel, BoxLayout.Y_AXIS));
+
+            JScrollPane scrollPane = new JScrollPane(listPanel);
+            scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+            scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+            this.add(scrollPane, BorderLayout.CENTER);
+        }
+
         // placeholder to make visualizing panel easier
         @Override
         protected void paintComponent(Graphics g) {
