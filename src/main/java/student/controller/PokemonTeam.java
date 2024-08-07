cb8cdfbdfd3357fcea2659c7fa40884020e46366
karyn-tan <tan.fe@northeastern.edu>
2024-08-07 01:39:32 -0700

    cleaning up pokemon team

--- src/main/java/student/controller/PokemonTeam.java
+++ src/main/java/student/controller/PokemonTeam.java
@@ -5,8 +5,6 @@ import student.model.PokeRecord;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Scanner; // USED FOR TESTING. PLS REMOVE @ FINAL SUBMISSION.
-
 
 /**
  * The PokemonTeam class allows you to manage a Pokemon team.
@@ -49,7 +47,6 @@ public class PokemonTeam {
             if (team.isEmpty()) {
                 System.out.println("Your team is empty.");
             } else {
-                System.out.println("Your team:");
                 for (PokeRecord pokemon : team) {
                     System.out.println(pokemon.name() + " (ID: " + pokemon.id() + ")");
                 }
@@ -61,27 +58,13 @@ public class PokemonTeam {
 
     /**
      * Adds a Pokemon to the team.
-     * Checks if the team already has 6 Pokemon or if the Pokemon is already in the team.
      *
      * @param name The name of the Pokemon to add.
      */
     public void addPokemonToTeam(String name) {
         try {
-            List<PokeRecord> team = model.getAllPokemonInTeam();
-
-            if (team.size() >= 6) { // checks if team is full
-                throw new Exception("Team is full. You cannot add more than 6 Pokemon.");
-            }
-
-            for (PokeRecord pokemon : team) { // checks if Pokemon is already in the team
-                if (pokemon.name().equalsIgnoreCase(name)) {
-                    throw new Exception(name + " is already in the team. You cannot add the same Pokemon twice.");
-                }
-            }
-
-            PokeRecord pokemon = model.getPokemonByName(name); // adds Pokemon to team
+            PokeRecord pokemon = model.getPokemonByName(name);
             model.addPokemonToTeam(pokemon);
-            System.out.println(pokemon.name() + " added to the team.");
         } catch (Exception e) {
             System.out.println("Error adding Pokemon: " + e.getMessage());
         }
@@ -96,41 +79,9 @@ public class PokemonTeam {
         try {
             PokeRecord pokemon = model.getPokemonFromTeamByName(name);
             model.removePokemonFromTeam(pokemon);
-            System.out.println(pokemon.name() + " removed from the team.");
         } catch (IOException e) {
             System.out.println("Error removing Pokemon: " + e.getMessage());
         }
     }
-
-    // CREATED JUST TO TEST THIS CLASS. NOT PART OF THE ORIGINAL CODE. BUT U CAN UNCOMMENT TO TEST.
-    // public static void main(String[] args) {
-    //     PokemonTeam teamManager = new PokemonTeam();
-    //     Scanner scanner = new Scanner(System.in);
-    //     String command;
-
-    //     while (true) {
-    //         System.out.println("commands: list, team, add [name], remove [name], exit");
-    //         System.out.print("enter command: ");
-    //         command = scanner.nextLine().trim();
-
-    //         if (command.equals("list")) {
-    //             teamManager.displayAllPokemon();
-    //         } else if (command.equals("team")) {
-    //             teamManager.displayTeam();
-    //         } else if (command.startsWith("add ")) {
-    //             String name = command.substring(4).trim();
-    //             teamManager.addPokemonToTeam(name);
-    //         } else if (command.startsWith("remove ")) {
-    //             String name = command.substring(7).trim();
-    //             teamManager.removePokemonFromTeam(name);
-    //         } else if (command.equals("exit")) {
-    //             break;
-    //         } else {
-    //             System.out.println("invalid command.");
-    //         }
-    //     }
-
-    //     scanner.close();
-    // }
 }
 
